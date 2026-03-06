75a69709d1b848d0f8303463453572130e7e3ed7
leanny <lalvenog@miumg.edu.gt>
2026-03-06 11:06:37 -0600

    ejercicio no8

---
+++ src/ejercicios/Ejercicio8.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+	
+	static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		
+		System.out.println("ingrese la hora: ");
+		int hora = scanner.nextInt();
+		
+		if(hora >= 6 && hora < 12) {
+			System.out.println("mañana");
+		}else if(hora >= 12 && hora < 18) {
+			System.out.println("tarde ");		
+		}else if(hora >= 18 && hora < 24) {
+			System.out.println("noche ");
+		}else {
+			System.out.println("hora invalida ");
+		}
+		scanner.close();
+	}
+}
